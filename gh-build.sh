8683961f9c86a869368af31c1cabdd2c0370e361
Uri Herrera <hello@uriherrera.com>
2024-12-18 15:52:00 -0600

    cleanup when building pkg

--- gh-build.sh
+++ gh-build.sh
@@ -6,6 +6,65 @@ set -x
 apt -qq update
 apt -qq -yy install equivs git devscripts lintian --no-install-recommends
 
+### Remove files and directories fomr upstream source.
+files=(
+    "*.asc" "*.asm" "*.c" "*.cmake" ".directory" "*.diff" "*.h" "*.py" "*.sh" "*.yaml" "*.yml"
+
+    "*[Ll][Ii][Cc][Ee][Nn][Cc][Ee]*"
+    "*Makefile*"
+    "*[Nn][Oo][Tt][Ii][Cc][Ee]*.txt"
+    "*WHENCE*"
+
+    "*iwlwifi-*"
+    "lib/firmware/.codespell.cfg"
+    "lib/firmware/.editorconfig"
+    "lib/firmware/.gitignore"
+    "lib/firmware/Apache-2"
+    "lib/firmware/Dockerfile"
+    "lib/firmware/GPL-2"
+    "lib/firmware/GPL-3"
+    "lib/firmware/README.md"
+    "lib/firmware/amd-ucode/README"
+    "lib/firmware/atusb/ChangeLog"
+    "lib/firmware/av7110/Boot.S"
+    "lib/firmware/av7110/Makefile"
+    "lib/firmware/brcm/brcmfmac43340-sdio.Insyde-VESPA2.txt"
+    "lib/firmware/brcm/brcmfmac43362-sdio.ASUSTeK COMPUTER INC.-ME176C.txt"
+    "lib/firmware/brcm/brcmfmac43430-sdio.ilife-S806.txt"
+    "lib/firmware/brcm/brcmfmac4354-sdio.nvidia,p2371-2180.txt"
+    "lib/firmware/contrib/templates/debian.changelog"
+    "lib/firmware/contrib/templates/debian.control"
+    "lib/firmware/contrib/templates/debian.copyright"
+    "lib/firmware/contrib/templates/rpm.spec"
+    "lib/firmware/isci/README"
+)
+
+
+directories=(
+    "lib/firmware/amd"
+    "lib/firmware/amd-ucode"
+    "lib/firmware/amdgpu"
+    "lib/firmware/amdnpu"
+    "lib/firmware/amdtee"
+    "lib/firmware/ar3k"
+    "lib/firmware/brcm"
+    "lib/firmware/carl9170fw"
+    "lib/firmware/cirrus"
+    "lib/firmware/nvidia"
+)
+
+echo "Removing files..."
+for file in "${files[@]}"; do
+    find . -type f -name "$file" -exec rm -f {} +
+done
+
+echo "Removing directories..."
+for dir in "${directories[@]}"; do
+    rm -rf "$dir"
+done
+
+echo "Cleanup complete."
+
 ### Install Dependencies
 mk-build-deps -i -t "apt-get --yes" -r
 
---
+++ lib/firmware/copy-firmware.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copy firmware files based on WHENCE list
+#
+
+verbose=:
+# shellcheck disable=SC2209
+compress=cat
+compext=
+destdir=
+
+err() {
+    printf "ERROR: %s\n" "$*"
+    exit 1
+}
+
+warn() {
+    printf "WARNING: %s\n" "$*"
+}
+
+while test $# -gt 0; do
+    case $1 in
+        -v | --verbose)
+            # shellcheck disable=SC2209
+            verbose=echo
+            shift
+            ;;
+
+        --xz)
+            if test "$compext" = ".zst"; then
+                err "cannot mix XZ and ZSTD compression"
+            fi
+            compress="xz --compress --quiet --stdout --check=crc32"
+            compext=".xz"
+            shift
+            ;;
+
+        --zstd)
+            if test "$compext" = ".xz"; then
+                err "cannot mix XZ and ZSTD compression"
+            fi
+            # shellcheck disable=SC2209
+            compress="zstd --compress --quiet --stdout"
+            compext=".zst"
+            shift
+            ;;
+
+        *)
+            if test -n "$destdir"; then
+                err "unknown command-line options: $*"
+            fi
+
+            destdir="$1"
+            shift
+            ;;
+    esac
+done
+
+if test -z "$destdir"; then
+    err "destination directory was not specified"
+fi
+
+if test -d "$destdir"; then
+    find "$destdir" -type d -empty >/dev/null || warn "destination folder is not empty."
+fi
+
+if test -e .git/config; then
+    $verbose "Checking that WHENCE file is formatted properly"
+    ./check_whence.py || err "check_whence.py has detected errors."
+fi
+
+# shellcheck disable=SC2162 # file/folder name can include escaped symbols
+grep -E '^(RawFile|File):' WHENCE | sed -E -e 's/^(RawFile|File): */\1 /;s/"//g' | while read k f; do
+    install -d "$destdir/$(dirname "$f")"
+    $verbose "copying/compressing file $f$compext"
+    if test "$compress" != "cat" && test "$k" = "RawFile"; then
+        $verbose "compression will be skipped for file $f"
+        cat "$f" > "$destdir/$f"
+    else
+        $compress "$f" > "$destdir/$f$compext"
+    fi
+done
+
+# shellcheck disable=SC2162 # file/folder name can include escaped symbols
+grep -E '^Link:' WHENCE | sed -e 's/^Link: *//g;s/-> //g' | while read l t; do
+    directory="$destdir/$(dirname "$l")"
+    install -d "$directory"
+    target="$(cd "$directory" && realpath -m -s "$t")"
+    if test -e "$target"; then
+        $verbose "creating link $l -> $t"
+        ln -s "$t" "$destdir/$l"
+    else
+        $verbose "creating link $l$compext -> $t$compext"
+        ln -s "$t$compext" "$destdir/$l$compext"
+    fi
+done
+
+# Verify no broken symlinks
+if test "$(find "$destdir" -xtype l | wc -l)" -ne 0 ; then
+    err "Broken symlinks found:\\n$(find "$destdir" -xtype l)"
+fi
+
+exit 0
+
+# vim: et sw=4 sts=4 ts=4
